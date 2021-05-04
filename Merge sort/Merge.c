1cd32d085d228f77303c12c7da29f62dd5a514b1
Prashant214 <62204140+Prashant2104@users.noreply.github.com>
2021-05-04 12:23:30 +0530

    Add files via upload

---
+++ Merge sort/Merge.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int L[10000], R[10000];
+
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	i = 0; 
+	j = 0; 
+	k = l; 
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r) 
+	{
+		int m = l + (r - l) / 2;
+
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+void printArray(int A[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+}
+
+int main()
+{
+	srand((int)time(0));
+	double time_spent = 0.0;
+	clock_t start, end;
+	start = clock();
+	int arr_size;
+	arr_size = 1000;
+	int arr[1000];
+
+	for (int i = 0; i < arr_size; i++)
+	{
+		arr[i] = rand() % (arr_size * 10);
+	}
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
+	end = clock();
+	time_spent += (double)(end - start) / CLOCKS_PER_SEC;
+	printf("\n\n Time = %f seconds", (time_spent));
+	return 0;
+}
